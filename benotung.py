30f592d44f3357c6e8626d0ec8abaef0a524a116
02sandro07 <sa5679@mci4me.at>
2024-02-09 12:32:24 +0100

    added table for benotung and updated queries from db

--- benotung.py
+++ benotung.py
@@ -26,31 +26,41 @@ def button_export_clicked():
 
 def uebersicht():
     db = DB()
-    buecher = db.query("SELECT name FROM Book")  # returns tuples
 
+    buecher = db.query("SELECT name FROM Book")  # returns tuples
     if buecher != []:
         buecher = [buch[0] for buch in buecher]  # convert to list
 
-    selected_book = container.selectbox(label="Buch auswählen", key="key_ausg_Buch", index=0, options=buecher,
-                                        help="Bitte hier das Buch auswählen das Sie anzeigen wollen!")
+
+    selected_book = container.selectbox(label="Buch auswählen", key="key_ausg_Buch", index=0, options=buecher, help="Bitte hier das Buch auswählen das Sie anzeigen wollen!")
+
 
     seitenanz_aus_DB = db.query("SELECT pages FROM Book WHERE name = ?", (selected_book,))
-    if seitenanz_aus_DB == []:
-        seitenanz_aus_DB = 0
-    else:
+    if seitenanz_aus_DB and seitenanz_aus_DB[0]:
         seitenanz_aus_DB = seitenanz_aus_DB[0][0]
+        container.write(F"Seitenanzahl: {seitenanz_aus_DB}")
+    else:
+        container.write("Das Buch hat keine Seitenanzahl.")
 
-    container.write(F"Seitenanzahl: {seitenanz_aus_DB}")
 
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Fach1", "Fach2", "Fach3"])
-    container.line_chart(chart_data)
+
+    df = pd.DataFrame(np.random.randn(10, seitenanz_aus_DB), columns=[f"S. {i}" for i in range(1, seitenanz_aus_DB + 1)])
+
+    st.table(df)
+
+
+
+
     container.write("Aktuelle Noten:")
     # notenstand ermitteln
     container.write("Fach 1: 1.45")
     container.write("Fach 2: 2.01")
     container.write("Fach 3: 3.23")
+
     container.button(label="Exportieren", on_click=button_export_clicked,
                      help="Klicken Sie hier um diese Übersicht zu exportieren!")
+
+
 
 
 if st.session_state.showSession == 1:
