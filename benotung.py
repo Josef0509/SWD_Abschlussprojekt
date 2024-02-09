09e3cad767e89ec4fdf60ce0c262b0a77e0d5aac
02sandro07 <sa5679@mci4me.at>
2024-02-09 12:06:49 +0100

    updated query in db.py, rough interface benotung.py

--- benotung.py
+++ benotung.py
@@ -1,9 +1,59 @@
 from st_pages import Page, show_pages, add_page_title
 import streamlit as st
+import pandas as pd
+import numpy as np
+from db import DB
 
+# Initialize session state
+if "showSession" not in st.session_state:
+    st.session_state.showSession = 1
 
 add_page_title()
 
+cl1, cl2, cl3 = st.columns([0.15, 0.15, 0.7])
+
+button1_ph = cl1.button("Übersicht", on_click=lambda: st.session_state.__setitem__("showSession", 1), help="Klicken Sie hier um zur Benotungsübersicht zu gelangen!")
+
+container = st.container()
 
 
+def button_export_clicked():
+    with st.spinner("Daten werden exportiert..."):
+        # Daten wirklich exportieren
+        pass
+    st.success("Erfolgsmeldung")
 
+
+def uebersicht():
+    db = DB()
+    buecher = db.query("SELECT name FROM Book")  # returns tuples
+
+    if buecher != []:
+        buecher = [buch[0] for buch in buecher]  # convert to list
+
+    selected_book = container.selectbox(label="Buch auswählen", key="key_ausg_Buch", index=0, options=buecher,
+                                        help="Bitte hier das Buch auswählen das Sie anzeigen wollen!")
+
+    seitenanz_aus_DB = db.query("SELECT pages FROM Book WHERE name = ?", (selected_book,))
+    if seitenanz_aus_DB == []:
+        seitenanz_aus_DB = 0
+    else:
+        seitenanz_aus_DB = seitenanz_aus_DB[0][0]
+
+    container.write(F"Seitenanzahl: {seitenanz_aus_DB}")
+
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Fach1", "Fach2", "Fach3"])
+    container.line_chart(chart_data)
+    container.write("Aktuelle Noten:")
+    # notenstand ermitteln
+    container.write("Fach 1: 1.45")
+    container.write("Fach 2: 2.01")
+    container.write("Fach 3: 3.23")
+    container.button(label="Exportieren", on_click=button_export_clicked,
+                     help="Klicken Sie hier um diese Übersicht zu exportieren!")
+
+
+if st.session_state.showSession == 1:
+    uebersicht()
+else:
+    pass
