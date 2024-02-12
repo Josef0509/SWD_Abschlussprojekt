1d979024f3540cac3c4a3af2afc95e94fa71b5c0
02sandro07 <sa5679@mci4me.at>
2024-02-12 15:27:22 +0100

    Überischt mit Noten zu sehen

--- benotung.py
+++ benotung.py
@@ -37,97 +37,109 @@ def uebersicht():
     st.header("Benotungsübersicht")
     db = DB()
 
+    selected_book = st.session_state.key_ausg_Buch
     
-
     seitenanz_aus_DB = db.query("SELECT pages FROM Book WHERE name = ?", (selected_book,))
     seitenanz_aus_DB = seitenanz_aus_DB[0][0] if seitenanz_aus_DB and seitenanz_aus_DB[0] else None
 
-    
+    book_id = db.query("SELECT bookID FROM Book WHERE name = ?", (selected_book,))
+    book_id = book_id[0][0]
+        
 
     kids = db.query("SELECT firstname, lastname FROM Kid")     #returns tuples
-    if kids != []:
-        kids = [kid[0]+" "+kid[1] for kid in kids]   #convert to list
 
+    if kids != []: 
+         kids = [kid[0]+" "+kid[1] for kid in kids]   #convert to list
+
+    df = pd.DataFrame()
 
     if seitenanz_aus_DB:
         container.write(f"Seitenanzahl: {seitenanz_aus_DB}")
 
+        # Fetch all kidIDs
+        kid_ids = db.query("SELECT kidID FROM Kid")
+        #container.write(kid_ids)
 
+        for page in range(1, seitenanz_aus_DB + 1):
+            page_grades = []
 
-        # Fetch kid names and create a DataFrame with random data
-        kids = db.query("SELECT firstname, lastname FROM Kid")
-
-        if kids != []:
-
-            kids = [f"{kid[0]} {kid[1]}" for kid in kids]
-            
-            #Make DataFrame for table
-            df = pd.DataFrame(columns=[f"S. {i}" for i in range(1, seitenanz_aus_DB + 1)])
-            df.insert(0, 'Kid Name', kids)  # Insert Kid Names as the first column
- 
-           
-            # Create a copy of the DataFrame with an additional column for row-selections
-            df_with_selections = df.copy()
-            df_with_selections.insert(0, "Select", False)
-
-
-            # Get dataframe row-selections from user with st.data_editor
-            edited_df = st.data_editor(
-                df_with_selections,
-                hide_index=True,
-                column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-                disabled=df.columns,
-            )
-
-            # Get selected rows from the edited DataFrame
-            selected_rows = edited_df[edited_df.Select]
-            
-            # Remove the selection column from the selected rows
-            selection = selected_rows.drop('Select', axis=1)
-
-            #extract the selected kids
-
-            selected_kid = selection['Kid Name']
-            selected_kid = [kid.split()[0] for kid in selected_kid]
-            print(selected_kid)
-
-            st.session_state.key_ausg_kid = selected_kid
-
-            
-
-            
-
-            #transponiere die Auswahl und füge die Spalte "Select" hinzu
-            selection = selection.transpose()
-            selection.insert(0, "Select", False)
-            selection.insert(0, "Seite", selection.index)
-
-            #erstelle ein data editor für die ausgewählten Seiten
-            edited_df_page = st.data_editor(
-                selection,
-                hide_index=True,
-                column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-                width=1000,
+            for kid_id in kid_ids:
+                #mache eine neue spalte für die noten von dieer seite
+                grade = db.query("SELECT grade FROM Grade WHERE kidID = ? AND bookID = ? AND page = ?", (kid_id[0], book_id, page))
+                if grade:
+                    page_grades.append(grade[0][0])
+                else:
+                    page_grades.append(None)
                 
-            )
+            df[f"S. {page}"] = page_grades
 
-            # Get selected rows from the edited DataFrame
-            selected_rows_page = edited_df_page[edited_df_page.Select]
-            # Remove the selection column from the selected rows
-            selection = selected_rows_page.drop('Select', axis=1)
+        df.insert(0, 'Kid Name', kids)  # Insert Kid Names as the first column
+
+        
+
+
+        # Create a copy of the DataFrame with an additional column for row-selections
+        df_with_selections = df.copy()
+        df_with_selections.insert(0, "Select", False)
+
+
+        # Get dataframe row-selections from user with st.data_editor
+        edited_df = st.data_editor(
+            df_with_selections,
+            hide_index=True,
+            column_config={"Select": st.column_config.CheckboxColumn(required=True)},
+            disabled=df.columns,
+        )
+
+        # Get selected rows from the edited DataFrame
+        selected_rows = edited_df[edited_df.Select]
+        
+        # Remove the selection column from the selected rows
+        selection = selected_rows.drop('Select', axis=1)
+
+        #extract the selected kids
+
+        selected_kid = selection['Kid Name']
+        selected_kid = [kid.split()[0] for kid in selected_kid]
+        print(selected_kid)
+
+        st.session_state.key_ausg_kid = selected_kid
+
+        
+
+        
+
+        #transponiere die Auswahl und füge die Spalte "Select" hinzu
+        selection = selection.transpose()
+        selection.insert(0, "Select", False)
+        selection.insert(1, "Seite", selection.index)
+
+        #erstelle ein data editor für die ausgewählten Seiten
+        edited_df_page = st.data_editor(
+            selection,
+            hide_index=True,
+            column_config={"Select": st.column_config.CheckboxColumn(required=True)},
+            width=1000,
             
-            
-            #extract the selected pages
-            selected_page = selection['Seite']
+        )
 
-            selected_page = [int(page.split()[1]) for page in selected_page]
-            #konvertiere die seiten in int
-            selected_page = [int(page) for page in selected_page]
+        # Get selected rows from the edited DataFrame
+        selected_rows_page = edited_df_page[edited_df_page.Select]
+        # Remove the selection column from the selected rows
+        selection = selected_rows_page.drop('Select', axis=1)
+        
+        
+        #extract the selected pages
+        selected_page = selection['Seite']
 
-            st.session_state.key_ausg_page = selected_page
+        selected_page = [int(page.split()[1]) for page in selected_page]
+        #konvertiere die seiten in int
+        selected_page = [int(page) for page in selected_page]
 
-            print(selected_page)
-                        
+        st.session_state.key_ausg_page = selected_page
+
+        print(selected_page)
+                    
 
 
 
