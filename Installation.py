edae928ef7278dcdbd3a7ba8323a66354b4d8a4b
02sandro07 <sa5679@mci4me.at>
2024-02-24 16:49:12 +0100

    bug fix for group without kids, tried .exe

--- Installation.py
+++ Installation.py
@@ -3,17 +3,15 @@ import sys
 import os
 
 def clone_repository(repo_url, destination):
-
     try:
         subprocess.run(['git', 'clone', repo_url, destination], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
         return False
-    
     return True
 
 repo_url = "https://github.com/Josef0509/SWD_Abschlussprojekt.git"
-destination = input(r"Enter the destination path:").strip()  # Using raw string
+destination = input("Enter the destination path: ").strip()  # No need for raw string
 
 if clone_repository(repo_url, destination):
     print("Repository cloned successfully!")
@@ -21,13 +19,18 @@ else:
     print("Error cloning repository.")
 
 # Creating a venv there
-print("Creating a virtual environment...")
-subprocess.run([sys.executable, '-m', 'venv', os.path.join(destination, 'venv')], check=True)
+#print("Creating a virtual environment...")
+#venv_path = os.path.join(destination, 'venv')
+#subprocess.run([sys.executable, '-m', 'venv', venv_path], check=True)
 
 # Activating the virtual environment
-venv_activate_script = os.path.join(destination, 'venv', 'Scripts', 'activate.bat')
-activate_cmd = f'call "{venv_activate_script}" && pip install streamlit st_pages matplotlib'
-subprocess.run(activate_cmd, shell=True)
+#venv_activate_script = os.path.join(venv_path, 'Scripts', 'activate.bat')
+#activate_cmd = f'call "{venv_activate_script}" && pip install streamlit st_pages matplotlib'
+#subprocess.run(activate_cmd, shell=True)
+
+# Directly install the packages without venv
+subprocess.run([sys.executable, '-m', 'pip', 'install', 'streamlit', 'st_pages', 'matplotlib'], check=True)
+
 
 # Done
 print("Done.")
--- benotung.py
+++ benotung.py
@@ -265,6 +265,7 @@ def detailansicht():
         logging.exception('Fehler beim Laden der GruppenID!')
         st.write("Fehler beim Laden der GruppenID!")
 
+    
     try:
         #kid selection
         kids_in_group, kids_nowhere = db.load_kids_in_group_or_available(group_id)
@@ -272,197 +273,203 @@ def detailansicht():
         logging.exception('Fehler beim Laden der Kinder in der Gruppe!')
         st.write("Fehler beim Laden der Kinder in der Gruppe!")
     
-    selected_kid = container.selectbox(label="Kind auswählen", key="key_ausg_Kind", index=0, options=kids_in_group,
-                                            help="Bitte hier das Kind auswählen das Sie anzeigen wollen!")
-    
-    
-
-    #get kid names
-    first_name = selected_kid.split(" ")[0]
-    last_name = selected_kid.split(" ")[1]
-    
-    try:
-        #get kid_id
-        kid_id = db.query("SELECT kidID FROM Kid WHERE firstname = ? AND lastname = ?", (first_name, last_name,))
-        kid_id = kid_id[0][0]
-    except Exception as e:
-        logging.exception('Fehler beim Laden der KidID!')
-        st.write("Fehler beim Laden der KidID!")
-    
-    
-    try:#book selection
-        book_id_result = db.query("SELECT bookID FROM Book WHERE name = ?", (selected_book,))
-        book_id = book_id_result[0][0]
-    except Exception as e:
-        logging.exception('Fehler beim Laden der BuchID!')
-        st.write("Fehler beim Laden der BuchID!")
-    
+    if kids_in_group == []:
+        st.error("Es sind keine Kinder in dieser Gruppe.")
+        
+    else:
 
 
-    #count how many pages are already graded
+        selected_kid = container.selectbox(label="Kind auswählen", key="key_ausg_Kind", index=0, options=kids_in_group,
+                                                help="Bitte hier das Kind auswählen das Sie anzeigen wollen!")
+        
+        
 
-    #count_pages = db.query("SELECT COUNT(DISTINCT page) FROM Grade WHERE kidID = ? AND bookID = ?", (kid_id, book_id))
-    #count_pages = count_pages[0][0] if count_pages and count_pages[0] else None
-    #st.header(count_pages)
-
-    #if count_pages == 0:
-    #    count_pages = 1
-    
-    try:
-        #get the number of pages of the selected book
-        seitenanz_aus_DB = db.query("SELECT pages FROM Book WHERE name = ?", (selected_book,))
-        seitenanz_aus_DB = seitenanz_aus_DB[0][0] if seitenanz_aus_DB and seitenanz_aus_DB[0] else None
-    except Exception as e:
-        logging.exception('Fehler bei der Übergabe der Seitenanzahl des Buches!')
-        st.write("Fehler bei der Übergabe der Seitenanzahl des Buches!")
-    
-
-    #select first ungraded page
-    last_graded_page = db.query("SELECT MAX(assignmentID) FROM Grade WHERE kidID = ? AND bookID = ?", (kid_id, book_id))
-    last_graded_page = last_graded_page[0][0] 
-    if last_graded_page == None:
-        last_graded_page = 1
-
-    #größer als seitananzahl
-    if last_graded_page + 1 > seitenanz_aus_DB:
-        last_graded_page = seitenanz_aus_DB-1
-
-    #page selection, +1 for next page to grade
-    selected_assignment_name = container.text_input(label="Seite auswählen", key="key_ausg_Seite", value=last_graded_page+1, placeholder="Seite", help="Bitte hier die Seite auswählen die Sie anzeigen wollen!")
-    
+        #get kid names
+        first_name = selected_kid.split(" ")[0]
+        last_name = selected_kid.split(" ")[1]
+        
+        try:
+            #get kid_id
+            kid_id = db.query("SELECT kidID FROM Kid WHERE firstname = ? AND lastname = ?", (first_name, last_name,))
+            kid_id = kid_id[0][0]
+        except Exception as e:
+            logging.exception('Fehler beim Laden der KidID!')
+            st.write("Fehler beim Laden der KidID!")
+        
+        
+        try:#book selection
+            book_id_result = db.query("SELECT bookID FROM Book WHERE name = ?", (selected_book,))
+            book_id = book_id_result[0][0]
+        except Exception as e:
+            logging.exception('Fehler beim Laden der BuchID!')
+            st.write("Fehler beim Laden der BuchID!")
+        
 
 
+        #count how many pages are already graded
 
+        #count_pages = db.query("SELECT COUNT(DISTINCT page) FROM Grade WHERE kidID = ? AND bookID = ?", (kid_id, book_id))
+        #count_pages = count_pages[0][0] if count_pages and count_pages[0] else None
+        #st.header(count_pages)
 
+        #if count_pages == 0:
+        #    count_pages = 1
+        
+        try:
+            #get the number of pages of the selected book
+            seitenanz_aus_DB = db.query("SELECT pages FROM Book WHERE name = ?", (selected_book,))
+            seitenanz_aus_DB = seitenanz_aus_DB[0][0] if seitenanz_aus_DB and seitenanz_aus_DB[0] else None
+        except Exception as e:
+            logging.exception('Fehler bei der Übergabe der Seitenanzahl des Buches!')
+            st.write("Fehler bei der Übergabe der Seitenanzahl des Buches!")
+        
 
-    #get all assignment names of the selected book
-    assignment_names = db.query("SELECT name FROM Assignment WHERE bookID = ?", (book_id,))
-    assignment_names = [assignment[0] for assignment in assignment_names] if assignment_names else []
+        #select first ungraded page
+        last_graded_page = db.query("SELECT MAX(assignmentID) FROM Grade WHERE kidID = ? AND bookID = ?", (kid_id, book_id))
+        last_graded_page = last_graded_page[0][0] 
+        if last_graded_page == None:
+            last_graded_page = 1
 
-    if selected_assignment_name not in assignment_names:
-        st.write("Die Seite existiert nicht.")
-        st.button("Aufgabe hinzufügen", help="Klicken Sie hier um eine neue Aufgabe hinzuzufügen!", key="key_add_assignment")
-        if st.session_state.key_add_assignment:
-            db.query("INSERT INTO Assignment (bookID, name) VALUES (?, ?)", (book_id, selected_assignment_name))
-            st.experimental_rerun()
+        #größer als seitananzahl
+        if last_graded_page + 1 > seitenanz_aus_DB:
+            last_graded_page = seitenanz_aus_DB-1
+
+        #page selection, +1 for next page to grade
+        selected_assignment_name = container.text_input(label="Seite auswählen", key="key_ausg_Seite", value=last_graded_page+1, placeholder="Seite", help="Bitte hier die Seite auswählen die Sie anzeigen wollen!")
         
 
 
 
-    assignment_ID = db.query("SELECT assignmentID FROM Assignment WHERE bookID = ? AND name = ?", (book_id, selected_assignment_name))
-    assignment_ID = assignment_ID[0][0] if assignment_ID and assignment_ID[0] else None
 
 
-    #get the grade, comment, weight and date of the selected page
+        #get all assignment names of the selected book
+        assignment_names = db.query("SELECT name FROM Assignment WHERE bookID = ?", (book_id,))
+        assignment_names = [assignment[0] for assignment in assignment_names] if assignment_names else []
 
-    assignment_description = db.query("SELECT description FROM Assignment WHERE assignmentID = ?", (assignment_ID,))
-
-    grade_result = db.query("SELECT grade FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id,assignment_ID))
-    
-    comment_result = db.query("SELECT comment FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
-    
-    weight_result = db.query("SELECT weight FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
-    
-    date_result = db.query("SELECT date FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
-    
-
-    #visual appearance
-    st.divider()
-    st.header("Eingabe der Details:")
+        if selected_assignment_name not in assignment_names:
+            st.write("Die Seite existiert nicht.")
+            st.button("Aufgabe hinzufügen", help="Klicken Sie hier um eine neue Aufgabe hinzuzufügen!", key="key_add_assignment")
+            if st.session_state.key_add_assignment:
+                db.query("INSERT INTO Assignment (bookID, name) VALUES (?, ?)", (book_id, selected_assignment_name))
+                st.experimental_rerun()
+            
 
 
 
-    #------------------------INPUTS---------------------------------------------------------------------
-
-    if assignment_description==[]:
-        assignment_description = ""
-    else:
-        assignment_description = assignment_description[0][0]
-    #input for assignment description
-    st.text_area(label="Aufgabenbeschreibung", placeholder="Aufgabenbeschreibung", help="Bitte hier die Aufgabenbeschreibung eingeben", key="key_assignment_description_input", value=assignment_description)
+        assignment_ID = db.query("SELECT assignmentID FROM Assignment WHERE bookID = ? AND name = ?", (book_id, selected_assignment_name))
+        assignment_ID = assignment_ID[0][0] if assignment_ID and assignment_ID[0] else None
 
 
+        #get the grade, comment, weight and date of the selected page
 
-    #if no grade is present, set grade_result to 1
-    if grade_result==[]:
-        grade_result = "1"
-    else:
-        grade_result = grade_result[0][0]
-    #input for grade
-    
-    st.toggle(label="Krank", key="key_K_input", value=False, help="Klicken Sie hier wenn das Kind krank war!")
+        assignment_description = db.query("SELECT description FROM Assignment WHERE assignmentID = ?", (assignment_ID,))
 
-    if st.session_state.key_K_input == False:
-        if grade_result == "K":
-            grade_result = 1
-        st.number_input(label = "Note", key="key_grade_input", value=int(grade_result), placeholder="Note", help="Bitte hier die Note eintragen!", step=1, min_value=1, max_value=5)
-        grade_input = st.session_state.key_grade_input
-    else:
-        #st.session_state.key_K_input == False:
-        grade_input = "K"
-    
-    
-    
-    #if no comment is present, set comment_result to None
-    if comment_result==[]:
-        comment_result = ""
-    else:
-        comment_result = comment_result[0][0]
-    #input for comment
-    st.text_area(label="Kommentar", placeholder="Kommentar", help="Bitte hier ihren Kommentar eingeben", key="key_comment_input",value=comment_result)
-    
-
-    #if no weight is present, set weight_result to 0.0
-    if weight_result==[]:
-        weight_result = 100
-    else:
-        weight_result = weight_result[0][0]
-    #input for weight
+        grade_result = db.query("SELECT grade FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id,assignment_ID))
         
-   
-    if st.session_state.key_K_input == True:
-        st.write(F"Das Kind war krank: Die Aufgabe fließt nicht in die Benotung.")
-        weight_input = 999
+        comment_result = db.query("SELECT comment FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
+        
+        weight_result = db.query("SELECT weight FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
+        
+        date_result = db.query("SELECT date FROM Grade WHERE kidID = ? AND bookID = ? AND assignmentID = ?", (kid_id, book_id, assignment_ID))
+        
 
-    else:
-        if weight_result == 999:
+        #visual appearance
+        st.divider()
+        st.header("Eingabe der Details:")
+
+
+
+        #------------------------INPUTS---------------------------------------------------------------------
+
+        if assignment_description==[]:
+            assignment_description = ""
+        else:
+            assignment_description = assignment_description[0][0]
+        #input for assignment description
+        st.text_area(label="Aufgabenbeschreibung", placeholder="Aufgabenbeschreibung", help="Bitte hier die Aufgabenbeschreibung eingeben", key="key_assignment_description_input", value=assignment_description)
+
+
+
+        #if no grade is present, set grade_result to 1
+        if grade_result==[]:
+            grade_result = "1"
+        else:
+            grade_result = grade_result[0][0]
+        #input for grade
+        
+        st.toggle(label="Krank", key="key_K_input", value=False, help="Klicken Sie hier wenn das Kind krank war!")
+
+        if st.session_state.key_K_input == False:
+            if grade_result == "K":
+                grade_result = 1
+            st.number_input(label = "Note", key="key_grade_input", value=int(grade_result), placeholder="Note", help="Bitte hier die Note eintragen!", step=1, min_value=1, max_value=5)
+            grade_input = st.session_state.key_grade_input
+        else:
+            #st.session_state.key_K_input == False:
+            grade_input = "K"
+        
+        
+        
+        #if no comment is present, set comment_result to None
+        if comment_result==[]:
+            comment_result = ""
+        else:
+            comment_result = comment_result[0][0]
+        #input for comment
+        st.text_area(label="Kommentar", placeholder="Kommentar", help="Bitte hier ihren Kommentar eingeben", key="key_comment_input",value=comment_result)
+        
+
+        #if no weight is present, set weight_result to 0.0
+        if weight_result==[]:
             weight_result = 100
-        st.slider(label="Gewichtung/Maximale Punkte", key="key_weight_input", value=weight_result, min_value=50, max_value=150, step=50, help="Bitte hier die Gewichtung eintragen! [**Leicht:** 50, **Normal:** 100, **Schwer:** 150] Diese entspricht der maximal erreichbaren Punkteanzahl.")
-        st.write(F"Das Kind bekommt **{gradeTOPercentage(st.session_state.key_grade_input)/100*st.session_state.key_weight_input}** Punkte gutgeschrieben.")
-        weight_input = st.session_state.key_weight_input
+        else:
+            weight_result = weight_result[0][0]
+        #input for weight
+            
+    
+        if st.session_state.key_K_input == True:
+            st.write(F"Das Kind war krank: Die Aufgabe fließt nicht in die Benotung.")
+            weight_input = 999
 
-    #if no date is present, set date_result to today's date
-    if date_result == []:
-        date_result = datetime.date.today()
+        else:
+            if weight_result == 999:
+                weight_result = 100
+            st.slider(label="Gewichtung/Maximale Punkte", key="key_weight_input", value=weight_result, min_value=50, max_value=150, step=50, help="Bitte hier die Gewichtung eintragen! [**Leicht:** 50, **Normal:** 100, **Schwer:** 150] Diese entspricht der maximal erreichbaren Punkteanzahl.")
+            st.write(F"Das Kind bekommt **{gradeTOPercentage(st.session_state.key_grade_input)/100*st.session_state.key_weight_input}** Punkte gutgeschrieben.")
+            weight_input = st.session_state.key_weight_input
 
-    else:
-        # Extract the date string from the tuple
-        date_result = date_result[0][0]
-        date_string = date_result
-        # Convert the date string to a datetime.date object
-        date_result = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+        #if no date is present, set date_result to today's date
+        if date_result == []:
+            date_result = datetime.date.today()
 
-    #input for date
-    st.date_input(label="Datum", help="Bitte hier das Datum eingeben", key="key_date_input", value=date_result)
+        else:
+            # Extract the date string from the tuple
+            date_result = date_result[0][0]
+            date_string = date_result
+            # Convert the date string to a datetime.date object
+            date_result = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+
+        #input for date
+        st.date_input(label="Datum", help="Bitte hier das Datum eingeben", key="key_date_input", value=date_result)
 
 
-    #------------------------BUTTONS---------------------------------------------------------------------
-    #buttons for saving, updating and deleting the grade
-    button_col1, button_col2, button_col3 = st.columns([0.3, 1, 1])
+        #------------------------BUTTONS---------------------------------------------------------------------
+        #buttons for saving, updating and deleting the grade
+        button_col1, button_col2, button_col3 = st.columns([0.3, 1, 1])
 
 
-    if button_col1.button("Speichern", help="Klicken Sie hier um die Note zu speichern!"):
-        db.update_or_save_grade(kid_id, book_id, assignment_ID, grade_input, st.session_state.key_comment_input, weight_input, st.session_state.key_date_input, st.session_state.key_assignment_description_input)
-        #seite neu laden
-        st.experimental_rerun()
-        
+        if button_col1.button("Speichern", help="Klicken Sie hier um die Note zu speichern!"):
+            db.update_or_save_grade(kid_id, book_id, assignment_ID, grade_input, st.session_state.key_comment_input, weight_input, st.session_state.key_date_input, st.session_state.key_assignment_description_input)
+            #seite neu laden
+            st.experimental_rerun()
+            
 
-    if button_col2.button("Löschen", help="Klicken Sie hier um die Note zu löschen!"):
-        db.delete_grade(kid_id, book_id, assignment_ID)
-        #seite neu laden
-        st.experimental_rerun()
+        if button_col2.button("Löschen", help="Klicken Sie hier um die Note zu löschen!"):
+            db.delete_grade(kid_id, book_id, assignment_ID)
+            #seite neu laden
+            st.experimental_rerun()
 
-    db.__del__()
+        db.__del__()
 
 
 
