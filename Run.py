22bff34355c43b944bbf2d6b748727a6fbd6e921
02sandro07 <sa5679@mci4me.at>
2024-02-25 11:56:04 +0100

    a

--- Run.py
+++ Run.py
@@ -5,16 +5,25 @@ def get_input_directory():
     try:
         with open("config.txt", "r") as file:
             input_directory = file.readline().strip()
+            print(f"Input directory: {input_directory}")
             return input_directory
     except FileNotFoundError:
         print("config.txt not found. Please run Installation.py first.")
         return None
 
 def main():
-    input_directory = get_input_directory()
+    # Setze das Arbeitsverzeichnis auf das Verzeichnis des ursprünglichen Skripts
+    print("Pfad zum Projektordner wird geladen...")
+    input_directory = os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    print(f"Working directory: {input_directory}")
+
+
+    #print("Pfad zum Projektordner wird geladen...")
+    #input_directory = get_input_directory()
 
     if input_directory:
         # Konstruiere den vollen Pfad zum Unterordner
+        print("Konstruiere den vollen Pfad zum Unterordner...")
         project_directory = os.path.join(input_directory, "SWD_Abschlussprojekt")
 
         # Überprüfe, ob der Projektordner existiert
@@ -22,8 +31,7 @@ def main():
             print(f"Project directory '{project_directory}' not found.")
             return
 
-        # Setze das Arbeitsverzeichnis auf das Verzeichnis des ursprünglichen Skripts
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        
 
         # PowerShell-Befehle vorbereiten
         powershell_commands = [
